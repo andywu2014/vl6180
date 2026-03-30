d1057b0322238390d52535b947171d1b5b1743e8
andy <andywu2014@foxmail.com>
2026-03-30 15:53:34 +0800

    modify accessibility

--- vl6180.ts
+++ vl6180.ts
@@ -20,8 +20,8 @@ namespace VL6180 {
     //% newAddr.min=0x07 newAddr.max=0x77 newAddr.defl=0x29
     //% block="初始化VL6180 || ，并新设地址为 %newAddr"
     //% weight=100
-    export function initVL6180(newAddr?: number): void {
-        newAddr = newAddr ? newAddr:0x29
+    export function initVL6180(newAddr?: Addr): void {
+        newAddr = newAddr ? newAddr : DefaultAddr
         // wait for Hardware standby and DeviceBooted
         basic.pause(2)
         if (newAddr != DefaultAddr) {
@@ -233,187 +233,190 @@ namespace VL6180 {
 
         writeInt8(addr, SYSRANGE__PART_TO_PART_RANGE_OFFSET, targetDis - Math.round(sum / 10))
     }
-}
 
-type Addr = number
+    // --- private---
 
-const DefaultAddr = 0x29
+    type Addr = number
 
-// reg addr
-const SYSRANGE__START = 0x18
-const RESULT__INTERRUPT_STATUS_GPIO = 0x4F
-const RESULT__RANGE_VAL = 0x62
-const SYSTEM__INTERRUPT_CLEAR = 0x15
-const SYSTEM__FRESH_OUT_OF_RESET = 0x16
-const I2C_SLAVE__DEVICE_ADDRESS = 0x212
-const SYSTEM__INTERRUPT_CONFIG_GPIO = 0x14
-const SYSRANGE__THRESH_HIGH = 0x019
-const SYSRANGE__THRESH_LOW = 0x019
-const SYSRANGE__INTERMEASUREMENT_PERIOD = 0x01B
-const READOUT__AVERAGING_SAMPLE_PERIOD = 0x10A
-const SYSRANGE__MAX_CONVERGENCE_TIME = 0x01C
-const RESULT__RANGE_STATUS = 0x04D
-const SYSTEM__HISTORY_CTRL = 0x012
-const RESULT__HISTORY_BUFFER_x = 0x052
-const SYSRANGE__PART_TO_PART_RANGE_OFFSET = 0x024
+    const DefaultAddr = 0x29
 
-const CONFIG_GPIO_INTERRUPT_NEW_SAMPLE_READY = 0x04
-/** clear ranging interrupt in write to #SYSTEM_INTERRUPT_CLEAR */
-const INTERRUPT_CLEAR_RANGING = 0x01
-/** clear als interrupt  in write to #SYSTEM_INTERRUPT_CLEAR */
-const INTERRUPT_CLEAR_ALS = 0x02
-/** clear error interrupt in write to #SYSTEM_INTERRUPT_CLEAR */
-const INTERRUPT_CLEAR_ERROR = 0x04
-const RANGE_DEVICE_READY_MASK = 0x01
+    // reg addr
+    const SYSRANGE__START = 0x18
+    const RESULT__INTERRUPT_STATUS_GPIO = 0x4F
+    const RESULT__RANGE_VAL = 0x62
+    const SYSTEM__INTERRUPT_CLEAR = 0x15
+    const SYSTEM__FRESH_OUT_OF_RESET = 0x16
+    const I2C_SLAVE__DEVICE_ADDRESS = 0x212
+    const SYSTEM__INTERRUPT_CONFIG_GPIO = 0x14
+    const SYSRANGE__THRESH_HIGH = 0x019
+    const SYSRANGE__THRESH_LOW = 0x019
+    const SYSRANGE__INTERMEASUREMENT_PERIOD = 0x01B
+    const READOUT__AVERAGING_SAMPLE_PERIOD = 0x10A
+    const SYSRANGE__MAX_CONVERGENCE_TIME = 0x01C
+    const RESULT__RANGE_STATUS = 0x04D
+    const SYSTEM__HISTORY_CTRL = 0x012
+    const RESULT__HISTORY_BUFFER_x = 0x052
+    const SYSRANGE__PART_TO_PART_RANGE_OFFSET = 0x024
 
-let VL6180Inited = control.allocateEventSource()
+    const CONFIG_GPIO_INTERRUPT_NEW_SAMPLE_READY = 0x04
+    /** clear ranging interrupt in write to #SYSTEM_INTERRUPT_CLEAR */
+    const INTERRUPT_CLEAR_RANGING = 0x01
+    /** clear als interrupt  in write to #SYSTEM_INTERRUPT_CLEAR */
+    const INTERRUPT_CLEAR_ALS = 0x02
+    /** clear error interrupt in write to #SYSTEM_INTERRUPT_CLEAR */
+    const INTERRUPT_CLEAR_ERROR = 0x04
+    const RANGE_DEVICE_READY_MASK = 0x01
 
-// Initialize sensor with settings from ST application note AN4545, section
-// "SR03 settings" - "Mandatory : private registers"
-function initVL6180_impl(i2caddr: Addr) {
-    if (read1Byte(i2caddr, SYSTEM__FRESH_OUT_OF_RESET) == 1) {
-        // "Mandatory : private registers"
-         
-        write1Byte(i2caddr, 0x207, 0x01);
-        write1Byte(i2caddr, 0x208, 0x01);
-        write1Byte(i2caddr, 0x096, 0x00);
-        // RANGE_SCALER = 253
-        write1Byte(i2caddr, 0x097, 0xFD);
-        write1Byte(i2caddr, 0x0E3, 0x01);
-        write1Byte(i2caddr, 0x0E4, 0x03);
-        write1Byte(i2caddr, 0x0E5, 0x02);
-        write1Byte(i2caddr, 0x0E6, 0x01);
-        write1Byte(i2caddr, 0x0E7, 0x03);
-        write1Byte(i2caddr, 0x0F5, 0x02);
-        write1Byte(i2caddr, 0x0D9, 0x05);
-        write1Byte(i2caddr, 0x0DB, 0xCE);
-        write1Byte(i2caddr, 0x0DC, 0x03);
-        write1Byte(i2caddr, 0x0DD, 0xF8);
-        write1Byte(i2caddr, 0x09F, 0x00);
-        write1Byte(i2caddr, 0x0A3, 0x3C);
-        write1Byte(i2caddr, 0x0B7, 0x00);
-        write1Byte(i2caddr, 0x0BB, 0x3C);
-        write1Byte(i2caddr, 0x0B2, 0x09);
-        write1Byte(i2caddr, 0x0CA, 0x09);
-        write1Byte(i2caddr, 0x198, 0x01);
-        write1Byte(i2caddr, 0x1B0, 0x17);
-        write1Byte(i2caddr, 0x1AD, 0x00);
-        write1Byte(i2caddr, 0x0FF, 0x05);
-        write1Byte(i2caddr, 0x100, 0x05);
-        write1Byte(i2caddr, 0x199, 0x05);
-        write1Byte(i2caddr, 0x1A6, 0x1B);
-        write1Byte(i2caddr, 0x1AC, 0x3E);
-        write1Byte(i2caddr, 0x1A7, 0x1F);
-        write1Byte(i2caddr, 0x030, 0x00);
+    let VL6180Inited = control.allocateEventSource()
 
-        // "Recommended : Public registers"
+    // Initialize sensor with settings from ST application note AN4545, section
+    // "SR03 settings" - "Mandatory : private registers"
+    function initVL6180_impl(i2caddr: Addr) {
+        if (read1Byte(i2caddr, SYSTEM__FRESH_OUT_OF_RESET) == 1) {
+            // "Mandatory : private registers"
 
-        // readout__averaging_sample_period = 48
-        write1Byte(i2caddr, READOUT__AVERAGING_SAMPLE_PERIOD, 0x30);
-        
-        // Reset other settings to power-on defaults
+            write1Byte(i2caddr, 0x207, 0x01);
+            write1Byte(i2caddr, 0x208, 0x01);
+            write1Byte(i2caddr, 0x096, 0x00);
+            // RANGE_SCALER = 253
+            write1Byte(i2caddr, 0x097, 0xFD);
+            write1Byte(i2caddr, 0x0E3, 0x01);
+            write1Byte(i2caddr, 0x0E4, 0x03);
+            write1Byte(i2caddr, 0x0E5, 0x02);
+            write1Byte(i2caddr, 0x0E6, 0x01);
+            write1Byte(i2caddr, 0x0E7, 0x03);
+            write1Byte(i2caddr, 0x0F5, 0x02);
+            write1Byte(i2caddr, 0x0D9, 0x05);
+            write1Byte(i2caddr, 0x0DB, 0xCE);
+            write1Byte(i2caddr, 0x0DC, 0x03);
+            write1Byte(i2caddr, 0x0DD, 0xF8);
+            write1Byte(i2caddr, 0x09F, 0x00);
+            write1Byte(i2caddr, 0x0A3, 0x3C);
+            write1Byte(i2caddr, 0x0B7, 0x00);
+            write1Byte(i2caddr, 0x0BB, 0x3C);
+            write1Byte(i2caddr, 0x0B2, 0x09);
+            write1Byte(i2caddr, 0x0CA, 0x09);
+            write1Byte(i2caddr, 0x198, 0x01);
+            write1Byte(i2caddr, 0x1B0, 0x17);
+            write1Byte(i2caddr, 0x1AD, 0x00);
+            write1Byte(i2caddr, 0x0FF, 0x05);
+            write1Byte(i2caddr, 0x100, 0x05);
+            write1Byte(i2caddr, 0x199, 0x05);
+            write1Byte(i2caddr, 0x1A6, 0x1B);
+            write1Byte(i2caddr, 0x1AC, 0x3E);
+            write1Byte(i2caddr, 0x1A7, 0x1F);
+            write1Byte(i2caddr, 0x030, 0x00);
 
-        // sysrange__max_convergence_time = 49 (49 ms)
-        write1Byte(i2caddr, SYSRANGE__MAX_CONVERGENCE_TIME, 0x31);
+            // "Recommended : Public registers"
 
-        // Thresh and open interrupt
+            // readout__averaging_sample_period = 48
+            write1Byte(i2caddr, READOUT__AVERAGING_SAMPLE_PERIOD, 0x30);
 
-        // set range interrupt
-        write1Byte(i2caddr, SYSTEM__INTERRUPT_CONFIG_GPIO, CONFIG_GPIO_INTERRUPT_NEW_SAMPLE_READY)
-        // range: 8~200 
-        write1Byte(i2caddr, SYSRANGE__THRESH_HIGH, 200)
-        write1Byte(i2caddr, SYSRANGE__THRESH_LOW, 8)
-        // clear all interrupt
-        write1Byte(i2caddr, SYSTEM__INTERRUPT_CLEAR, INTERRUPT_CLEAR_ERROR | INTERRUPT_CLEAR_RANGING | INTERRUPT_CLEAR_ALS)
+            // Reset other settings to power-on defaults
 
-        // enable history
-        write1Byte(i2caddr, SYSTEM__HISTORY_CTRL, 0x01)
+            // sysrange__max_convergence_time = 49 (49 ms)
+            write1Byte(i2caddr, SYSRANGE__MAX_CONVERGENCE_TIME, 0x31);
 
-        write1Byte(i2caddr, SYSTEM__FRESH_OUT_OF_RESET, 0)
+            // Thresh and open interrupt
+
+            // set range interrupt
+            write1Byte(i2caddr, SYSTEM__INTERRUPT_CONFIG_GPIO, CONFIG_GPIO_INTERRUPT_NEW_SAMPLE_READY)
+            // range: 8~200 
+            write1Byte(i2caddr, SYSRANGE__THRESH_HIGH, 200)
+            write1Byte(i2caddr, SYSRANGE__THRESH_LOW, 8)
+            // clear all interrupt
+            write1Byte(i2caddr, SYSTEM__INTERRUPT_CLEAR, INTERRUPT_CLEAR_ERROR | INTERRUPT_CLEAR_RANGING | INTERRUPT_CLEAR_ALS)
+
+            // enable history
+            write1Byte(i2caddr, SYSTEM__HISTORY_CTRL, 0x01)
+
+            write1Byte(i2caddr, SYSTEM__FRESH_OUT_OF_RESET, 0)
+        }
+
+        // stop. This will actually start a single measurement of range
+        // if continuous mode is not active, so it's a good idea to
+        // wait a few hundred ms after calling this function to let that complete
+        // before starting continuous mode again or taking a reading.
+        write1Byte(i2caddr, SYSRANGE__START, 1)
+        basic.pause(100)
     }
 
-    // stop. This will actually start a single measurement of range
-    // if continuous mode is not active, so it's a good idea to
-    // wait a few hundred ms after calling this function to let that complete
-    // before starting continuous mode again or taking a reading.
-    write1Byte(i2caddr, SYSRANGE__START, 1)
-    basic.pause(100)
-}
-
-function write1Byte(i2caddr: Addr, reg: number, value: number) {
-    let buf = pins.createBuffer(3)
-    buf.setNumber(NumberFormat.UInt16BE, 0, reg)
-    buf.setNumber(NumberFormat.UInt8BE, 2, value)
-    pins.i2cWriteBuffer(i2caddr, buf)
-}
-
-function writeInt8(i2caddr: Addr, reg: number, value: number) {
-    let buf = pins.createBuffer(3)
-    buf.setNumber(NumberFormat.UInt16BE, 0, reg)
-    buf.setNumber(NumberFormat.Int8BE, 2, value)
-    pins.i2cWriteBuffer(i2caddr, buf)
-}
-
-function read1Byte(i2caddr: Addr, reg: number): number {
-    pins.i2cWriteNumber(
-        i2caddr,
-        reg,
-        NumberFormat.UInt16BE,
-        true
-    )
-    return pins.i2cReadNumber(i2caddr, NumberFormat.UInt8BE, false)
-}
-
-function readInt8(i2caddr: Addr, reg: number): number {
-    pins.i2cWriteNumber(
-        i2caddr,
-        reg,
-        NumberFormat.UInt16BE,
-        true
-    )
-    return pins.i2cReadNumber(i2caddr, NumberFormat.Int8BE, false)
-}
-
-function readToBuffer(i2caddr: Addr, reg: number, size: number): Buffer {
-    pins.i2cWriteNumber(
-        i2caddr,
-        reg,
-        NumberFormat.UInt16BE,
-        true
-    )
-    return pins.i2cReadBuffer(i2caddr, size, false)
-}
-
-function waitARange(addr: Addr):number {
-    // pre-cal + ct of 50mm 88% + readout
-    const delay = 3.2 + 0.24 + 4.3
-    basic.pause(delay)
-    while ((read1Byte(addr, RESULT__INTERRUPT_STATUS_GPIO)
-        & CONFIG_GPIO_INTERRUPT_NEW_SAMPLE_READY) == 0) {
-        basic.pause(1)
+    function write1Byte(i2caddr: Addr, reg: number, value: number) {
+        let buf = pins.createBuffer(3)
+        buf.setNumber(NumberFormat.UInt16BE, 0, reg)
+        buf.setNumber(NumberFormat.UInt8BE, 2, value)
+        pins.i2cWriteBuffer(i2caddr, buf)
     }
-    let ret = read1Byte(addr, RESULT__RANGE_VAL)
-    write1Byte(addr, SYSTEM__INTERRUPT_CLEAR, INTERRUPT_CLEAR_RANGING)
-    return ret
-}
 
-function waitRangReady(addr: Addr):void {
-    while ((read1Byte(addr, RESULT__RANGE_STATUS) & RANGE_DEVICE_READY_MASK) == 0) {
-        basic.pause(10)
+    function writeInt8(i2caddr: Addr, reg: number, value: number) {
+        let buf = pins.createBuffer(3)
+        buf.setNumber(NumberFormat.UInt16BE, 0, reg)
+        buf.setNumber(NumberFormat.Int8BE, 2, value)
+        pins.i2cWriteBuffer(i2caddr, buf)
+    }
+
+    function read1Byte(i2caddr: Addr, reg: number): number {
+        pins.i2cWriteNumber(
+            i2caddr,
+            reg,
+            NumberFormat.UInt16BE,
+            true
+        )
+        return pins.i2cReadNumber(i2caddr, NumberFormat.UInt8BE, false)
+    }
+
+    function readInt8(i2caddr: Addr, reg: number): number {
+        pins.i2cWriteNumber(
+            i2caddr,
+            reg,
+            NumberFormat.UInt16BE,
+            true
+        )
+        return pins.i2cReadNumber(i2caddr, NumberFormat.Int8BE, false)
+    }
+
+    function readToBuffer(i2caddr: Addr, reg: number, size: number): Buffer {
+        pins.i2cWriteNumber(
+            i2caddr,
+            reg,
+            NumberFormat.UInt16BE,
+            true
+        )
+        return pins.i2cReadBuffer(i2caddr, size, false)
+    }
+
+    function waitARange(addr: Addr): number {
+        // pre-cal + ct of 50mm 88% + readout
+        const delay = 3.2 + 0.24 + 4.3
+        basic.pause(delay)
+        while ((read1Byte(addr, RESULT__INTERRUPT_STATUS_GPIO)
+            & CONFIG_GPIO_INTERRUPT_NEW_SAMPLE_READY) == 0) {
+            basic.pause(1)
+        }
+        let ret = read1Byte(addr, RESULT__RANGE_VAL)
+        write1Byte(addr, SYSTEM__INTERRUPT_CLEAR, INTERRUPT_CLEAR_RANGING)
+        return ret
+    }
+
+    function waitRangReady(addr: Addr): void {
+        while ((read1Byte(addr, RESULT__RANGE_STATUS) & RANGE_DEVICE_READY_MASK) == 0) {
+            basic.pause(10)
+        }
+    }
+
+    function readLatestRange(addr: Addr): number {
+        return read1Byte(addr, RESULT__HISTORY_BUFFER_x)
+    }
+
+    function isContinualMode(addr: Addr): boolean {
+        return (read1Byte(addr, SYSRANGE__START) & 0x02) != 0
+    }
+
+    function startASingleMeasurement(addr: Addr): number {
+        waitRangReady(addr)
+        write1Byte(addr, SYSRANGE__START, 1)
+        return waitARange(addr)
     }
 }
 
-function readLatestRange(addr: Addr): number {
-    return read1Byte(addr, RESULT__HISTORY_BUFFER_x)
-}
-
-function isContinualMode(addr: Addr): boolean {
-    return (read1Byte(addr, SYSRANGE__START) & 0x02) != 0
-}
-
-function startASingleMeasurement(addr: Addr): number {
-    waitRangReady(addr)
-    write1Byte(addr, SYSRANGE__START, 1)
-    return waitARange(addr)
-}
 
